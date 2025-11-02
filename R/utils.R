072b5f5ea5f378ed3f44b13f9ed7379f808b9d1d
Bruce1998a <120488092+Bruce1998a@users.noreply.github.com>
2025-11-02 18:55:02 -0500

    Delete R directory

--- R/utils.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-var_name <- function(variable) {
-  switch(as.integer(variable),
-         "Precipitacion (mm)", "Temperatura media (°C)",
-         "Temperatura maxima (°C)", "Temperatura minima (°C)")
-}
-
-#' @keywords internal
-.is_numeric <- function(x) is.numeric(x) && !is.factor(x)
